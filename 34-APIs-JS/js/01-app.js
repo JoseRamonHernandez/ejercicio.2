0448af250a91cb842cbf0d9bc1cf95f1ba9f933c
JoseRamonHernandez <67391682+JoseRamonHernandez@users.noreply.github.com / joseramonhernandezmunoz20@gmail.com>
2020-09-14 19:56:45 -0500

    Video 213
    Notification

---
+++ 34-APIs-JS/js/01-app.js
@@ -0,0 +1,30 @@
+const notificarBtn = document.querySelector('#notificar');
+
+notificarBtn.addEventListener('click', () => {
+    Notification
+        .requestPermission()
+        .then( resultado  => 
+            {
+                console.log('El resultado es: ', resultado);
+            })
+}); 
+
+const verNotificacion = document.querySelector('#verNotificacion');
+
+verNotificacion.addEventListener('click', () =>{
+    //console.log('lo oprimiste');
+    if(Notification.permission === 'granted')
+    {
+       // console.log('lo oprimiste');
+       const notificacion = new Notification('Esta es la notificacion al seleccionar granted',
+       {
+           icon: 'img/ccj.png',
+           body: 'Codigo'
+       });
+
+       notificacion.onclick = function ( ) {
+           window.open('https://www.codigoconjuan.com')
+       }
+    }
+    
+});
